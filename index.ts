57f79789ccf6cc3d8ccdbfb2fcc55a1dffffeb09
Ankush Verma <ankushverma@wiznis.com>
2024-12-08 21:16:33 +0530

    main: enable cors

--- index.ts
+++ index.ts
@@ -1,16 +1,25 @@
-import express, { query, Request, Response } from "express";
+import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
-import routes  from "./routes";
+import routes from "./routes";
 import { syncModels } from "./repository/syncModels";
+import cors from "cors";
+
 const app = express();
 const port = process.env.API_PORT;
+
+// Enable CORS for all origins
+app.use(cors());
+
+// Body Parser Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/api",routes);
+
+// API Routes
+app.use("/api", routes);
 
 app.listen(port, async () => {
     console.log(`Server running on http://localhost:${port}`);
-    console.log('syncing models');
+    console.log('Syncing models...');
     await syncModels();
 });
